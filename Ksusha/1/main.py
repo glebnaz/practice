531dee025720f45cf824414886d5920763e12dee
glebnaz <nazemnovgleb@gmail.com>
2019-12-08 14:08:36 +0400

    first task done

--- Ksusha/1/main.py
+++ Ksusha/1/main.py
@@ -1,7 +1,10 @@
 from typeOfCredit import types
 from client import client
+from credit import credit
 
 t = types("Деловой","Быть Деловым Мальчиком",15,"12.10.2020")
 c = client("Сгу","Государственная","Астраханская 83","500-599","Чумаченко")
+cr = credit(t,c,"152154","9.10.2018")
 print(t)
 print(c)
+print(cr)
