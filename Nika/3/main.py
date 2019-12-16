bf9a266b2bda7c6264bcfa305cc9302bc5b1c560
Gleb Nazemnov <nazemnovgleb@gmail.com>
2019-12-16 13:22:53 +0400

    refactor

--- Nika/3/main.py
+++ Nika/3/main.py
@@ -1,6 +1,7 @@
 import xml.dom.minidom
 from dataxml import dataxml
 
-parser = dataxml("data.xml")
-
+parser = dataxml("/Users/glebnazemnov/Documents/#main/#WORK/sgu /4/oop/practice/Nika/3/data.xml")
+parser.parseData()
+parser.writeData()
 parser.writeDb("newDb.sqlite")
